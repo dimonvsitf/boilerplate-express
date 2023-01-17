9834c40666727ce708f07a7f84a132945680000b
dimonvsitf <zczldva@ucl.ac.uk>
2023-01-18 04:44:28 +0900

    exercises

--- myApp.js
+++ myApp.js
@@ -1,6 +1,20 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+app.use("/public", express.static(__dirname+"/public/"));
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function(req,res) {
+    res.json({
+        "message": "Hello json"
+    });
+});
+
 
 
 
