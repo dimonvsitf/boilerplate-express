03b9cbb9af3b101d424fa7026e4cabc822299df4
dimonvsitf <zczldva@ucl.ac.uk>
2023-01-18 05:54:12 +0900

    na

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
+
 console.log("Hello World");
+
+app.use('/',(req,res,next) =>{
+    console.log(req.method+' '+req.path+' - '+req.ip);
+    next();
+});
 
 app.use("/public", express.static(__dirname+"/public/"));
 
@@ -9,11 +16,14 @@ app.get("/", function(req,res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+
+
 app.get("/json", function(req,res) {
-    res.json({
-        "message": "Hello json"
-    });
-});
+    
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "Hello json".toUpperCase()})
+    } else res.json({"message": "Hello json"})        
+});    
 
 
 
